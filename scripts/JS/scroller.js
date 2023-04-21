0b7b4b6cce20575d523cbddf896a7008484bf90b
Din Eilie (דין אילי) <98718983+DinEilie@users.noreply.github.com>
2023-04-22 02:31:00 +0300

    Update - 22.04.2023

--- scripts/JS/scroller.js
+++ scripts/JS/scroller.js
@@ -1,3 +1,6 @@
+// var scroller: number = window.scrollY;
+var windowHeight = window.innerHeight;
+var revealFromTop = 120;
 var scroller = window.scrollY;
 const duration = 800;
 const bgTitle = document.getElementById("bg-title");
@@ -12,63 +15,62 @@ const about3 = document.getElementById("about3");
 const about4 = document.getElementById("about4");
 const about5 = document.getElementById("about5");
 const about6 = document.querySelectorAll(".fill-bg");
-console.log("Scroller Y:" + scroller);
+console.log("projects:" + projects.offsetTop);
+console.log("project1:" + project1.offsetTop);
+console.log("project2:" + project2.offsetTop);
 // Listen to scroller Y axis
 window.addEventListener("scroll", (event) => {
     scroller = this.scrollY;
-    console.log("Scroller Y:" + scroller);
-    console.log("projects:" + projects.offsetTop);
-    console.log("about:" + about.offsetTop);
+    windowHeight = this.innerHeight;
     if (screen.width > 1920) {
         bgTitle.innerHTML = "P R O J E C T S";
-        // Toggle arrow button
-        if (scroller <= 405) {
-            arrowUp.classList.remove("fadeInDown");
-            arrowUp.hidden = true;
-        }
-        else {
-            arrowUp.classList.add("fadeInDown");
-            arrowUp.hidden = false;
-        }
-        // Toggle arrow button
-        if (scroller >= projects.offsetTop - 600) {
-            project1.classList.remove("invisible");
-            project1.classList.add("fadeInRight");
-        }
-        if (scroller >= projects.offsetTop - 150) {
-            project2.classList.remove("invisible");
-            project2.classList.add("fadeInLeft");
-        }
-        // Toggle about
-        if (scroller >= about.offsetTop - 900) {
-            bgTitle.innerHTML = '<span class="ps-5">C O N T A C T</span>';
-            about1.classList.remove("invisible");
-            about1.classList.add("fadeInDown");
-            setTimeout(() => {
-                about3.classList.remove("invisible");
-                about3.classList.add("fadeInDown");
-            }, 500);
-            setTimeout(() => {
-                about4.classList.remove("invisible");
-                about4.classList.add("fadeInDown");
-            }, 1000);
-            setTimeout(() => {
-                about5.classList.remove("invisible");
-                about5.classList.add("fadeInDown");
-            }, 1500);
-            setTimeout(() => {
-                for (let i = 0; i < about6.length; i++) {
-                    about6[i].classList.remove("text-white");
-                    about6[i].classList.add("text-dark");
-                    about6[i].classList.add("scale-in-hor-center");
-                }
-            }, 2000);
-        }
+        // // Toggle arrow button
+        // if (scroller <= 405) {
+        //   arrowUp.classList.remove("fadeInDown");
+        //   arrowUp.hidden = true;
+        // } else {
+        //   arrowUp.classList.add("fadeInDown");
+        //   arrowUp.hidden = false;
+        // }
+        // // Toggle arrow button
+        // if (scroller >= projects.offsetTop - 600) {
+        //   project1.classList.remove("invisible");
+        //   project1.classList.add("fadeInRight");
+        // }
+        // if (scroller >= projects.offsetTop - 150) {
+        //   project2.classList.remove("invisible");
+        //   project2.classList.add("fadeInLeft");
+        // }
+        // // Toggle about
+        // if (scroller >= about.offsetTop - 900) {
+        //   bgTitle.innerHTML = '<span class="ps-5">C O N T A C T</span>';
+        //   about1.classList.remove("invisible");
+        //   about1.classList.add("fadeInDown");
+        //   setTimeout(() => {
+        //     about3.classList.remove("invisible");
+        //     about3.classList.add("fadeInDown");
+        //   }, 500);
+        //   setTimeout(() => {
+        //     about4.classList.remove("invisible");
+        //     about4.classList.add("fadeInDown");
+        //   }, 1000);
+        //   setTimeout(() => {
+        //     about5.classList.remove("invisible");
+        //     about5.classList.add("fadeInDown");
+        //   }, 1500);
+        //   setTimeout(() => {
+        //     for (let i = 0; i < about6.length; i++) {
+        //       about6[i].classList.remove("text-white");
+        //       about6[i].classList.add("text-dark");
+        //       about6[i].classList.add("scale-in-hor-center");
+        //     }
+        //   }, 2000);
+        // }
     }
     else if (screen.width <= 1920 && screen.width > 1440) {
         bgTitle.innerHTML = "P R O J E C T S";
         // Toggle arrow button
-        if (scroller <= 350) {
+        if (scroller <= 365) {
             arrowUp.classList.remove("fadeInDown");
             arrowUp.hidden = true;
         }
@@ -77,16 +79,16 @@ window.addEventListener("scroll", (event) => {
             arrowUp.hidden = false;
         }
         // Toggle projects blocks
-        if (scroller >= projects.offsetTop - 365) {
+        if (project1.getBoundingClientRect().top <= revealFromTop * 2) {
             project1.classList.remove("invisible");
             project1.classList.add("fadeInRight");
         }
-        if (scroller >= projects.offsetTop + 150) {
+        if (project2.getBoundingClientRect().top <= revealFromTop * 2) {
             project2.classList.remove("invisible");
             project2.classList.add("fadeInLeft");
         }
         // Toggle about
-        if (scroller >= about.offsetTop - 460) {
+        if (about.getBoundingClientRect().top <= revealFromTop / 4) {
             bgTitle.innerHTML = '<span class="ps-5">C O N T A C T</span>';
             about1.classList.remove("invisible");
             about1.classList.add("fadeInDown");
--- scripts/TS/scroller.ts
+++ scripts/TS/scroller.ts
@@ -1,4 +1,7 @@
-var scroller: number = window.scrollY;
+// var scroller: number = window.scrollY;
+var windowHeight: number = window.innerHeight;
+var revealFromTop: number = 120;
+var scroller = window.scrollY;
 const duration: number = 800;
 const bgTitle: HTMLElement = document.getElementById("bg-title"); 
 const home: HTMLElement = document.getElementById("home");
@@ -12,71 +15,71 @@ const about3: HTMLElement = document.getElementById("about3");
 const about4: HTMLElement = document.getElementById("about4");
 const about5: HTMLElement = document.getElementById("about5");
 const about6: NodeListOf<Element> = document.querySelectorAll(".fill-bg");
-console.log("Scroller Y:" + scroller);
+console.log("projects:" + projects.offsetTop);
+console.log("project1:" + project1.offsetTop);
+console.log("project2:" + project2.offsetTop);
 
 // Listen to scroller Y axis
 window.addEventListener("scroll", (event) => {
   scroller = this.scrollY;
-  console.log("Scroller Y:" + scroller);
-  console.log("projects:" + projects.offsetTop);
-  console.log("about:" + about.offsetTop);
+  windowHeight = this.innerHeight;
   if (screen.width > 1920) {
     bgTitle.innerHTML = "P R O J E C T S";
     
-    // Toggle arrow button
-    if (scroller <= 405) {
-      arrowUp.classList.remove("fadeInDown");
-      arrowUp.hidden = true;
-    } else {
-      arrowUp.classList.add("fadeInDown");
-      arrowUp.hidden = false;
-    }
+    // // Toggle arrow button
+    // if (scroller <= 405) {
+    //   arrowUp.classList.remove("fadeInDown");
+    //   arrowUp.hidden = true;
+    // } else {
+    //   arrowUp.classList.add("fadeInDown");
+    //   arrowUp.hidden = false;
+    // }
 
-    // Toggle arrow button
-    if (scroller >= projects.offsetTop - 600) {
-      project1.classList.remove("invisible");
-      project1.classList.add("fadeInRight");
-    }
-    if (scroller >= projects.offsetTop - 150) {
-      project2.classList.remove("invisible");
-      project2.classList.add("fadeInLeft");
-    }
+    // // Toggle arrow button
+    // if (scroller >= projects.offsetTop - 600) {
+    //   project1.classList.remove("invisible");
+    //   project1.classList.add("fadeInRight");
+    // }
+    // if (scroller >= projects.offsetTop - 150) {
+    //   project2.classList.remove("invisible");
+    //   project2.classList.add("fadeInLeft");
+    // }
 
-    // Toggle about
-    if (scroller >= about.offsetTop - 900) {
-      bgTitle.innerHTML = '<span class="ps-5">C O N T A C T</span>';
-      about1.classList.remove("invisible");
-      about1.classList.add("fadeInDown");
+    // // Toggle about
+    // if (scroller >= about.offsetTop - 900) {
+    //   bgTitle.innerHTML = '<span class="ps-5">C O N T A C T</span>';
+    //   about1.classList.remove("invisible");
+    //   about1.classList.add("fadeInDown");
 
-      setTimeout(() => {
-        about3.classList.remove("invisible");
-        about3.classList.add("fadeInDown");
-      }, 500);
+    //   setTimeout(() => {
+    //     about3.classList.remove("invisible");
+    //     about3.classList.add("fadeInDown");
+    //   }, 500);
 
-      setTimeout(() => {
-        about4.classList.remove("invisible");
-        about4.classList.add("fadeInDown");
-      }, 1000);
+    //   setTimeout(() => {
+    //     about4.classList.remove("invisible");
+    //     about4.classList.add("fadeInDown");
+    //   }, 1000);
 
-      setTimeout(() => {
-        about5.classList.remove("invisible");
-        about5.classList.add("fadeInDown");
-      }, 1500);
+    //   setTimeout(() => {
+    //     about5.classList.remove("invisible");
+    //     about5.classList.add("fadeInDown");
+    //   }, 1500);
 
-      setTimeout(() => {
-        for (let i = 0; i < about6.length; i++) {
-          about6[i].classList.remove("text-white");
-          about6[i].classList.add("text-dark");
-          about6[i].classList.add("scale-in-hor-center");
-        }
-      }, 2000);
-    }
+    //   setTimeout(() => {
+    //     for (let i = 0; i < about6.length; i++) {
+    //       about6[i].classList.remove("text-white");
+    //       about6[i].classList.add("text-dark");
+    //       about6[i].classList.add("scale-in-hor-center");
+    //     }
+    //   }, 2000);
+    // }
 
   } else if (screen.width <= 1920 && screen.width > 1440) {
     bgTitle.innerHTML = "P R O J E C T S";
 
     // Toggle arrow button
-    if (scroller <= 350) {
+    if (scroller <= 365) {
       arrowUp.classList.remove("fadeInDown");
       arrowUp.hidden = true;
     } else {
@@ -85,17 +88,17 @@ window.addEventListener("scroll", (event) => {
     }
 
     // Toggle projects blocks
-    if (scroller >= projects.offsetTop - 365) {
+    if (project1.getBoundingClientRect().top <= revealFromTop * 2) {
       project1.classList.remove("invisible");
       project1.classList.add("fadeInRight");
     }
-    if (scroller >= projects.offsetTop + 150) {
+    if (project2.getBoundingClientRect().top <= revealFromTop * 2) {
       project2.classList.remove("invisible");
       project2.classList.add("fadeInLeft");
     }
 
     // Toggle about
-    if (scroller >= about.offsetTop - 460) {
+    if (about.getBoundingClientRect().top <= 0) {
       bgTitle.innerHTML = '<span class="ps-5">C O N T A C T</span>';
       about1.classList.remove("invisible");
       about1.classList.add("fadeInDown");
