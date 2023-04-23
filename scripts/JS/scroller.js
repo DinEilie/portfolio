262c094228300d45aec065da11b50330246a276a
DinEilie <dineilie1997@gmail.com>
2023-04-23 19:52:57 +0300

    Update  - 23.04.2023

--- scripts/JS/scroller.js
+++ scripts/JS/scroller.js
@@ -24,14 +24,15 @@ window.addEventListener("scroll", (event) => {
     windowHeight = this.innerHeight;
     if (screen.width > 1920) {
         bgTitle.innerHTML = "P R O J E C T S";
-        // // Toggle arrow button
-        // if (scroller <= 405) {
-        //   arrowUp.classList.remove("fadeInDown");
-        //   arrowUp.hidden = true;
-        // } else {
-        //   arrowUp.classList.add("fadeInDown");
-        //   arrowUp.hidden = false;
-        // }
+        // Toggle arrow button
+        if (scroller <= 405) {
+            arrowUp.classList.remove("fadeInDown");
+            arrowUp.hidden = true;
+        }
+        else {
+            arrowUp.classList.add("fadeInDown");
+            arrowUp.hidden = false;
+        }
         // // Toggle arrow button
         // if (scroller >= projects.offsetTop - 600) {
         //   project1.classList.remove("invisible");
@@ -78,42 +79,42 @@ window.addEventListener("scroll", (event) => {
             arrowUp.classList.add("fadeInDown");
             arrowUp.hidden = false;
         }
-        // Toggle projects blocks
-        if (project1.getBoundingClientRect().top <= revealFromTop * 2) {
-            project1.classList.remove("invisible");
-            project1.classList.add("fadeInRight");
-        }
-        if (project2.getBoundingClientRect().top <= revealFromTop * 2) {
-            project2.classList.remove("invisible");
-            project2.classList.add("fadeInLeft");
-        }
-        // Toggle about
-        if (about.getBoundingClientRect().top <= revealFromTop / 4) {
-            bgTitle.innerHTML = '<span class="ps-5">C O N T A C T</span>';
-            about1.classList.remove("invisible");
-            about1.classList.add("fadeInDown");
-            setTimeout(() => {
-                about3.classList.remove("invisible");
-                about3.classList.add("fadeInDown");
-            }, 500);
-            setTimeout(() => {
-                about4.classList.remove("invisible");
-                about4.classList.add("fadeInDown");
-            }, 1000);
-            setTimeout(() => {
-                about5.classList.remove("invisible");
-                about5.classList.add("fadeInDown");
-            }, 1500);
-            setTimeout(() => {
-                for (let i = 0; i < about6.length; i++) {
-                    about6[i].classList.remove("text-white");
-                    about6[i].classList.add("text-dark");
-                    about6[i].classList.add("scale-in-hor-center");
-                }
-            }, 2000);
-        }
     }
     else if (screen.width <= 1440 && screen.width > 1200) {
+    }
+    // Toggle projects blocks
+    if (project1.getBoundingClientRect().top <= revealFromTop * 2) {
+        project1.classList.remove("invisible");
+        project1.classList.add("fadeInRight");
+    }
+    if (project2.getBoundingClientRect().top <= revealFromTop * 2) {
+        project2.classList.remove("invisible");
+        project2.classList.add("fadeInLeft");
+    }
+    // Toggle about
+    if (about.getBoundingClientRect().top <= 0) {
+        bgTitle.innerHTML = '<span class="ps-5">C O N T A C T</span>';
+        about1.classList.remove("invisible");
+        about1.classList.add("fadeInDown");
+        setTimeout(() => {
+            about3.classList.remove("invisible");
+            about3.classList.add("fadeInDown");
+        }, 500);
+        setTimeout(() => {
+            about4.classList.remove("invisible");
+            about4.classList.add("fadeInDown");
+        }, 1000);
+        setTimeout(() => {
+            about5.classList.remove("invisible");
+            about5.classList.add("fadeInDown");
+        }, 1500);
+        setTimeout(() => {
+            for (let i = 0; i < about6.length; i++) {
+                about6[i].classList.remove("text-white");
+                about6[i].classList.add("text-dark");
+                about6[i].classList.add("scale-in-hor-center");
+            }
+        }, 2000);
     }
 });
 //# sourceMappingURL=scroller.js.map
